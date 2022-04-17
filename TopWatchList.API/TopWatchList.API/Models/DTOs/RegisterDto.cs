2bd1226a32bf9614797b2bc6a1160945ebbfcc06
Paul Bailey <52pbailey@gmail.com>
2022-04-17 14:02:41 +0100

    feat: adding Register Dto

---
+++ TopWatchList.API/TopWatchList.API/Models/DTOs/RegisterDto.cs
@@ -0,0 +1,18 @@
+﻿namespace TopWatchList.API.Models.DTOs
+{
+    /// <summary>
+    /// A Dto to register a <see cref="User"/>.
+    /// </summary>
+    public class RegisterDto
+    {
+        /// <summary>
+        /// Gets or Sets the UserName of the <see cref="User"/>.
+        /// </summary>
+        public string UserName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or Sets the <see cref="User"/>s Password.
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+    }
+}
